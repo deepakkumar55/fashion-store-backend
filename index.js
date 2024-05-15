ad35804313d6b6519685789d50151669aca8a063
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2024-05-15 03:06:16 +0000

    cors changes

--- index.js
+++ index.js
@@ -17,7 +17,24 @@ var corsOptions = {
     }
   }
 }
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://fashion-store-frontend-dun.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
+ 
+  app.get('/api/category-product', async (req, res) => {
+    try {
+      const response = await fetch('https://fashion-store-backend-kixn.onrender.com/api/category-product');
+      const data = await response.json();
+      res.json(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: 'Failed to fetch data' });
+    }
+  });
 
 app.use(express.json())
 app.use(cookieParser())
